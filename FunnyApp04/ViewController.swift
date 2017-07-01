b3bad0620bd081ced2d7a1a541e0df450c3cdd1e
hanlyjiang <hanlyjiang@outlook.com>
2017-07-02 01:49:21 +0800

    添加label 和 按钮点击事件

--- FunnyApp04/ViewController.swift
+++ FunnyApp04/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func startRecord(_ sender: Any) {
+        recordingLabel.text = "正在录音..."
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "点击录音按钮开始录音"
+    }
 }
 
